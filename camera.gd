25ec2404514c7a8bd012e993cd8578418ac765b6
Jonathan Camarena <jonathancamarenacamcho@gmail.com>
2024-07-18 21:39:52 -0700

    something

---
+++ camera.gd
@@ -0,0 +1,13 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Hello!")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#position.x = position.x + 1
+	pass
